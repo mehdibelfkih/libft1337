5e8034a9f311e1accecae9059385b0a2174c6516
el mehdi belfkih <mehdibm.az@gmail.com>
2022-10-27 21:20:16 +0100

    28

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ebelfkih <ebelfkih@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 10:33:24 by ebelfkih          #+#    #+#             */
-/*   Updated: 2022/10/21 14:42:29 by ebelfkih         ###   ########.fr       */
+/*   Updated: 2022/10/27 21:19:08 by ebelfkih         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,22 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t	i;
+	size_t	i1;
+	size_t	i2;
 	size_t	j;
 
-	i = 0;
 	j = 0;
-	while (dst[i])
-		i++;
-	while (dstsize > i + 1)
+	i1 = ft_strlen(src);
+	i2 = ft_strlen(dst);
+	if (dstsize == 0)
+		return (i1);
+	while (dstsize > j + i2 + 1 && src[j])
 	{
-		dst[i] = src[j];
-		i++;
+		dst[j + i2] = src[j];
 		j++;
 	}
-		dst[i] = '\0';
-	return (i - 1);
+	dst[j + i2] = '\0';
+	if (dstsize > i2)
+		return (i1 + i2);
+	return (i1 + dstsize);
 }
